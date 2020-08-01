f60edce697d86dd372d7eb5aba1c1fd45f368cca
LeeRyangHwa <iryanghwa@gmail.com>
2020-08-01 15:37:54 +0900

    station_commit

--- src/main/java/com/algorist/bus_project/Controller/StationController.java
+++ src/main/java/com/algorist/bus_project/Controller/StationController.java
@@ -1,6 +1,5 @@
 package com.algorist.bus_project.Controller;
 
-import com.algorist.bus_project.Vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +25,6 @@ public class StationController {
 
         //값 로그 확인
         logger.info("######  x:"+x+"  y:"+y);
-
 
         return mapList;
     }
